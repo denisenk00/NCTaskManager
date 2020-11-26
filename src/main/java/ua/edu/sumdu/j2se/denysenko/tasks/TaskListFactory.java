d5fc1a5ca88b125bffc11f4877b70cd9c9daa285
denisenk00 <dmitriy.denisenko.2002@gmail.com>
2020-11-26 19:52:09 +0200

    Added new classes AbstractTaskList, ListTypes, TaskListFactory

---
+++ src/main/java/ua/edu/sumdu/j2se/denysenko/tasks/TaskListFactory.java
@@ -0,0 +1,16 @@
+package ua.edu.sumdu.j2se.denysenko.tasks;
+
+public class TaskListFactory {
+    public static AbstractTaskList createTaskList(ListTypes.types type){
+        switch (type){
+            case ARRAY:
+                ArrayTaskList arrayTaskList = new ArrayTaskList();
+                return arrayTaskList;
+            case LINKED:
+                LinkedTaskList linkedTaskList = new LinkedTaskList();
+                return linkedTaskList;
+            default:
+                return null;
+        }
+    }
+}
